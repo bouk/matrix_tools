697d727b95fa7c315ca38932cfe4fcbd4668e61a
Bouke van der Bijl <boukevanderbijl@gmail.com>
2013-06-11 15:38:51 +0200

    Switch over to O(N^3) algorithm

--- lib/matrix_tools/max_sum.rb
+++ lib/matrix_tools/max_sum.rb
@@ -3,38 +3,30 @@ require 'matrix'
 class Matrix
   # Uses O(N^4) dynamic programming algorithm
   def max_sum
-    # Field (row, column) of this array will contains the submatrix sum of (0, 0) to (row, column)
+    # Field (row, column) of this array will contains the sum of all fields in the rows above it in the same column
     sums = Array.new(row_size) { Array.new(column_size) { 0 } }
 
     (0...row_size).each do |row|
       (0...column_size).each do |column|
         sums[row][column] = element(row, column)
-
         sums[row][column] += sums[row - 1][column] if row > 0
-        sums[row][column] += sums[row][column - 1] if column > 0
-
-        # Make sure we don't count a submatrix double
-        sums[row][column] -= sums[row - 1][column - 1] if row > 0 && column > 0
       end
     end
 
-    # Go through all possible submatrices (which is O(N^4)) and calculate the sum
-    # of that submatrix, which is O(1) by using the sums array created above
+    # Use Kadan's algorithm to arrive at a O(N^3) submatrix algorithm
     best_sum = sums[0][0]
-    (0...row_size).each do |start_row|
-      (0...column_size).each do |start_column|
-        (start_row...row_size).each do |end_row|
-          (start_column...column_size).each do |end_column|
-            subsum = sums[end_row][end_column]
+    (-1...row_size).each do |start_row|
+      (start_row...row_size).each do |end_row|
+        current_sum = sums[end_row][0] - (if start_row == -1 then 0 else sums[start_row][0] end)
+        subrows_best_sum = current_sum
 
-            subsum -= sums[start_row - 1][end_column] if start_row > 0
-            subsum -= sums[end_row][start_column - 1] if start_column > 0
-
-            subsum += sums[start_row - 1][start_column - 1] if start_row > 0 && start_column > 0
-
-            best_sum = [best_sum, subsum].max
-          end
+        (1...column_size).each do |column|
+          subsum = sums[end_row][column] - (if start_row == -1 then 0 else sums[start_row][column] end)
+          current_sum = [subsum + current_sum, subsum].max
+          subrows_best_sum = [subrows_best_sum, current_sum].max
         end
+
+        best_sum = [best_sum, subrows_best_sum].max
       end
     end
 
